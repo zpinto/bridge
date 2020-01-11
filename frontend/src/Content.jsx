f9b4b40773485f6d15274e1bafaacd4c7c499087
Daania <daaniakalam.18@gmail.com>
2020-01-11 11:29:22 -0800

    Fixed merge conflict with daania

--- frontend/src/Content.jsx
+++ frontend/src/Content.jsx
@@ -6,6 +6,7 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Login from "./components/Login";
 import Register from "./components/Register";
+import JobDescriptionView from "./components/JobDecriptionView";
 
 const Content = props => {
   const { user, handleLogin } = props;
@@ -18,6 +19,8 @@ const Content = props => {
         </Route>
         <Route exact path="/register" component={Register} />
         <Route exact path="/about" component={About} />
+        <Route exact path="/JobDescriptionView" component={JobDescriptionView} />
+          <Route exact path="/testDK" component={JobPosting}/>
         <Route exact path="/portal">
           <h1>Application Portal</h1>
           <h2>Applicant</h2>
