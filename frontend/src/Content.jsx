d651cea8e3ad16768b4810aa3432fc484e0cad6e
Jean-Paul Nguyen <jean.paul_nguyen@yahoo.com>
2020-01-11 11:20:34 -0800

    Changed structure

---
+++ frontend/src/Content.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Home from "./components/Home";
+import About from "./components/About";
+import Login from "./components/Login";
+import Register from "./components/Register";
+
+const Content = props => {
+  const { user, handleLogin } = props;
+
+  return (
+    <Router>
+      <Switch>
+        <Route strict exact path="/">
+          {user ? <Home user={user} /> : <Login handleLogin={handleLogin} />}
+        </Route>
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/portal">
+          <h1>Application Portal</h1>
+          <h2>Applicant</h2>
+        </Route>
+        <Route exact path="/review">
+          <h1>Review Portal</h1>
+          <h2>Applicant</h2>
+        </Route>
+        <Route exact path="/job/add">
+          <h1>Add Job Page</h1>
+          <h2>Recruiter</h2>
+        </Route>
+        <Route exact path="/job/:id">
+          <h1>Job Page</h1>
+          <h2>Recruiter</h2>
+        </Route>
+        <Route>
+          <h1> ERROR </h1>
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
+
+export default Content;
