d07f47b84a8e9497512f69095efdf981efac49c5
Paul Yang <palyang777@gmail.com>
2020-01-11 02:05:02 -0800

    added jobclasses

---
+++ backend/job.py
@@ -0,0 +1,11 @@
+class JobApplication:
+    def __init__(self):
+        self._resume = None
+        self._job_post_id = None
+        self._user_id = None  
+
+class JobPost:
+    def __init__(self):
+        self._job_type = ""
+        self.company_name = ""
+        self.qualifications = []  
