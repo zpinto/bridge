e6b8fe91e2dd5d0c05311770bf3e57f3c9dc6487
Daania <daaniakalam.18@gmail.com>
2020-01-11 14:50:35 -0800

    not fully functional

---
+++ frontend/src/components/PeerResumeReview.jsx
@@ -0,0 +1,52 @@
+import React, { useState, component } from "react";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import { Box } from "@material-ui/core";
+import {Document, Page} from "react-pdf";
+
+const useStyles = makeStyles(theme => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center"
+  },
+  login: {
+    border: 1
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1)
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2)
+  }
+}));
+
+export default function PeerResumeReview() {
+    
+    const classes = useStyles();
+    const { pageNumber, numPages } = this.state;
+    
+    return (
+      <Container component="main" fullWidth alignItems="left">
+        <CssBaseline />
+        <div className={classes.paper}>
+        <Document
+              file="https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf"
+              onLoadSuccess={this.onDocumentLoadSuccess}
+            >
+              <Page pageNumber={pageNumber} />
+            </Document>
+            <p>Page {pageNumber} of {numPages}</p>
+        </div>
+      </Container>
+
+    );
+  }
