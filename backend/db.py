f29872c71d2121f1a1b56988a1c3be2f0ab9547a
Paul Yang <palyang777@gmail.com>
2020-01-11 01:52:19 -0800

    wrapped db ops

--- backend/db.py
+++ backend/db.py
@@ -1,3 +1,18 @@
 from google.cloud import firestore
 
 db = firestore.Client()
+
+def create(collection_name, data):
+    db.collection(collection_name).document().set(data)
+
+def read(collection_name, id):
+    return db.collection(collection_name).document(id).get()
+
+def update(collection_name, id, new_value):
+    db.collection(collection_name).document(id).update(new_value)
+
+def delete(collection_name, id):
+    db.collection(collection_name).document(id).delete()
+
+def where(collection_name, key, operation, value):
+    return db.collection(collection_name).where(key, operation, value)
